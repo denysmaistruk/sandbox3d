912fded7b11a14b4e09ee718284507f539840b2f
Sky Thirteenth <sky.13th.95@gmail.com>
2023-01-11 18:38:32 +0200

    fix texture filtering issue

--- src/graphics/text3d.cpp
+++ src/graphics/text3d.cpp
@@ -15,7 +15,7 @@ Shader LoadText3DShader()
 Font GetFontText3D()
 {
     static Font font = LoadFont(CASCADIA_FONT_PATH);
-    SetTextureFilter(font.texture, TEXTURE_FILTER_TRILINEAR);
+    SetTextureFilter(font.texture, RL_TEXTURE_FILTER_LINEAR);
     return font;
 }
 
